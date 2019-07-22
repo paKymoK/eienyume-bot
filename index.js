d2ee2a3ad91b76e3755bcf6f717e61ab3742ff00
Truong Quang Thai <seelevollereiggz@gmail.com>
2019-07-22 21:03:48 +0700

    init
    init

---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ */
+
+// Import the discord.js module
+const token = 'NjAyNTE3NzA2MTU1MjI5MTg1.XTSEyA.ilPWxHdr5G5l2l6tFdmUThthfeo'
+const Discord = require('discord.js');
+const { Client, Attachment,RichEmbed } = require('discord.js');
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find(ch => ch.name === 'new-comer-board');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+  });
+  client.on('message', message => {
+    let array = message.content.split(" ");
+    // If the message is "how to embed"
+    if (array[0] ==="!ping") {
+      // We can create embeds using the MessageEmbed constructor
+      // Read more about all that you can do with the constructor
+      // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+      const attachment = new Attachment('https://images-ext-2.discordapp.net/external/nNFNnlWkndSjjoDCTu8R69OhN6YC5dFaijaUnxuMtFc/https/cdn.discordapp.com/emojis/469349079105929226.png');
+      const embed = new RichEmbed()
+      .setAuthor("author name","https://images-ext-2.discordapp.net/external/nNFNnlWkndSjjoDCTu8R69OhN6YC5dFaijaUnxuMtFc/https/cdn.discordapp.com/emojis/469349079105929226.png")
+        // Set the title of the field
+        .setTitle(attachment)
+        // Set the color of the embed
+        .setColor(0xFF0000)
+        // Set the main content of the embed
+        .setDescription('Hello, this is a slick embed!');
+      // Send the embed to the same channel as the message
+      message.channel.send(embed);
+    }
+  });
+  
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(token);
