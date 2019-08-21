c748bc846cd638d02c084c4722a86acd8cdb334b
Truong Quang Thai <seelevollereiggz@gmail.com>
2019-08-22 01:26:39 +0700

    next

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
-
 const Discord = require('discord.js');
 require('dotenv').config();
-const { Client, Attachment, RichEmbed } = require('discord.js');
+const { Client, Attachment, RichEmbed, Permissions } = require('discord.js');
 const client = new Discord.Client();
+const permissions = new Discord.Permissions(8);
 var fs = require('fs');
 var json = JSON.parse(fs.readFileSync('./item.json', 'utf8'));
+var badword = JSON.parse(fs.readFileSync('./badword.json', 'utf8'));
 client.on('ready', () => {
   console.log('I am ready!');
+  client.user.setPresence({
+    game: {
+      name: 'với lửa',
+      type: 'PLAYING'
+    },
+    status: 'online'
+  })
 });
 client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find(ch => ch.name === 'new-comer-board');
+  const channel = member.guild.channels.find(ch => ch.name === 'new-comers-board');
   if (!channel) return;
   channel.send(`Welcome to the server, ${member}`);
 });
+
+
+// client.on("typingStart", function (channel, user) {
+//   if (channel.id == 603878285792772106) {
+//     console.log(`Chào mừng ${user.tag} đến với kênh thử nghiệm`);
+//     user.send('Chào mừng ``'+ `${user.tag}` +'`` đến với kênh thử nghiệm');
+//   }
+// });
 client.on('message', message => {
   let messageArray = message.content.split(" ");
+  messageArray.every(function (element) {
+    if (element === badword[1]) {
+      message.member.send('Nói ``' + badword[1] + '`` là hư')
+      return false;
+    }
+    return true;
+  })
+});
+
+//
+client.on('message', message => {
+  let messageArray = message.content.split(" ");
+  var searchString = "";
+  for (var i = 1; i < messageArray.length; i++) {
+    searchString = searchString + messageArray[i];
+  }
   var id = -1;
+  var count = 0;
+  var idlist = [];
   function logArrayElements(element, index) {
     let idArray = element.id.split(",");
     idArray.forEach(elements => {
@@ -25,9 +59,100 @@ client.on('message', message => {
       }
     });
   }
-  
-  // If the message is "how to embed"
-  if (messageArray[0] === "!vn") {
+  function logArrayElementsByName(element, index) {
+    let idArray = element.name.toLowerCase().split(" ");
+    var noSpace = "";
+    for (var i = 0; i < idArray.length; i++) {
+      noSpace = noSpace + idArray[i];
+    }
+    if (noSpace.indexOf(searchString) != -1) {
+      id = index;
+      count++;
+      idlist.push(id);
+    }
+  }
+  if (messageArray[0] === "!tênthật") {
+    message.channel.send(message.member.user.tag);
+  }
+  // tìm theo tên
+  if (messageArray[0] === "!tên") {
+    json.forEach(logArrayElementsByName);
+    if (count == 0) {
+      message.channel.send('Không có món nào tên như nềy cả !!! ')
+    }
+    if ((id != -1) && (count > 1)) {
+      var description = '';
+      idlist.forEach(element => {
+        console.log(element);
+        const setItem = client.emojis.find(emoji => emoji.name === json[element].set);
+        description = description + "Tên : " + setItem + " " + json[element].name + " `-` ID : " + json[element].id + " `-` Loại :" + json[element].type + "\n" + "\n"
+      })
+      if (description.length > 2000) {
+        message.channel.send('Từ này ngắn quá , làm ơn thêm chữ đi !!');
+      } else {
+        const embed = new RichEmbed()
+          .setTitle('Danh sách đồ có chứa từ : ' + messageArray[1])
+          .setFooter("Slave của Wanderer")
+          .setTimestamp()
+          .setDescription(description + "\n" +
+            "Gõ ``!id <ID>`` để xem chi tiết món đó" + "\n" +
+            "Ví dụ ``!id 3012``");
+        embed.setColor(0xFF0000)
+        message.channel.send(embed);
+      }
+    }
+    if ((id != -1) && (count == 1)) {
+      const setItem = client.emojis.find(emoji => emoji.name === json[id].set);
+      const starNumber = client.emojis.find(emoji => emoji.name === json[id].rarity);
+      const star = client.emojis.find(emoji => emoji.name === "star");
+      const attribute = client.emojis.find(emoji => emoji.name === json[id].attribute);
+      const embed = new RichEmbed()
+        .setTitle(setItem + " " + json[id].name)
+        .setFooter("Slave của Wanderer")
+        .setTimestamp()
+      //Nếu là Wea/badge/Armor thì add 
+      if (json[id].attribute === "") {
+        // Set the main content of the embed
+        embed.setDescription("Số sao: " + starNumber + star + "\n" + "\n" +
+          "Loại : " + json[id].type + "\n" + "\n" +
+          "ID : " + json[id].id + "\n" + "\n" +
+          "Set : " + setItem + " " + json[id].set
+        )
+      } else {
+        // Set the main content of the embed
+        embed.setDescription("Số sao: " + starNumber + star + "\n" + "\n" +
+          "Loại : " + json[id].type + "\n" + "\n" +
+          "ID : " + json[id].id + "\n" + "\n" +
+          "Thuộc tính :" + attribute + "\n" + "\n" +
+          "Set : " + setItem + " " + json[id].set
+        )
+      }
+      embed.setThumbnail(json[id].thumbnail)
+        .addField("Effect 1 : " + json[id].skill1name, json[id].skill1)
+        .addField("Effect 2 : " + json[id].skill2name, json[id].skill2)
+      if (json[id].type === "Áo") {
+        embed.addField("Chỉ số : ", "```" + "Cost : " + json[id].cost + "\n" +
+          "Máu cơ bản : " + json[id].basehp + "\n" + "```")
+      } else {
+        if (json[id].type === "Huy hiệu") {
+          embed.addField("Chỉ số : ", "```" + "Cost : " + json[id].cost + "\n" + "```")
+        } else {
+          //weapon
+          embed.addField("Chỉ số : ", "```" + "Cost : " + json[id].cost + "\n" +
+            "Sát thương : " + json[id].basedame + "\n"
+            + "Tốc độ : " + json[id].attackspeed + " /s" + "\n"
+            + "Đạn : " + json[id].ammo + "```")
+        }
+      }
+      if (json[id].moe === "Có") {
+        embed.setImage(json[id].linkmoe)
+      }
+      embed.setColor(0xFF0000)
+      message.channel.send(embed);
+    }
+  }
+  // tìm theo id
+  if (messageArray[0] === "!id") {
     json.forEach(logArrayElements);
     if (id != -1) {
       const setItem = client.emojis.find(emoji => emoji.name === json[id].set);
@@ -44,7 +169,7 @@ client.on('message', message => {
         embed.setDescription("Số sao: " + starNumber + star + "\n" + "\n" +
           "Loại : " + json[id].type + "\n" + "\n" +
           "ID : " + json[id].id + "\n" + "\n" +
-          "Set : " + setItem+" " + json[id].set
+          "Set : " + setItem + " " + json[id].set
         )
       } else {
         // Set the main content of the embed
@@ -52,7 +177,7 @@ client.on('message', message => {
           "Loại : " + json[id].type + "\n" + "\n" +
           "ID : " + json[id].id + "\n" + "\n" +
           "Thuộc tính :" + attribute + "\n" + "\n" +
-          "Set : " + setItem +" "+ json[id].set
+          "Set : " + setItem + " " + json[id].set
         )
       }
       embed.setThumbnail(json[id].thumbnail)
@@ -84,7 +209,3 @@ client.on('message', message => {
 });
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(process.env.MY_API_KEY);
-React.render(
-  <h1>Hello, world!</h1>,
-  document.body
-);
