8466581f0d7ad2e049cd7e74ddaca8eccb23adb9
Truong Quang Thai <seelevollereiggz@gmail.com>
2019-08-25 20:25:03 +0700

    fixed

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const permissions = new Discord.Permissions(8);
 var fs = require('fs');
 //fire base
 var admin = require("firebase-admin");
-
-var serviceAccount = require("./bot-discord-ggz-firebase-adminsdk-24d2f-11dc647e1a.json");
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": "bot-discord-ggz",
+    "private_key_id": JSON.stringify(process.env.private_key_id),
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQChuXPlzS0DgSCM\nEcfqdOtnXrvxJeGg/n+l8Lc+83v0eAv5UsPAZXabi3lcfJMdE7j+2vzpZN4X1ckL\nsNaz3dBreKwl7737qa1IeF/ZYrSKdbeLiHqw18ac6nolQrxWWS90cJ9nfzfgbWM/\nm+eSyNgbCJheG84/WD2hzgcSlnXzsi1OXUhTzbag0t6jKXie1nGZfg1CqymH4bQ+\nCxcep1czBzX3+xK+n+gdOtJH6rI4n2sKAl3APUZ2yTQm/I1DRnMeb6/Dfqxjoamf\nSCq7knfQGE2i3mFsNmCSj8OlF7yNFGBuopeUM6pEe2cXd1DVjApvI++c9COYbMvT\n/nGHnmg/AgMBAAECggEAAV1zMiYxsj0K8nO3TW4liy6nBR7jO9HPb632S1SQdXH1\nST+ewbHO4aWN5YRy/29VA+hYSHteo90yQGQtORZvCxaD1CF6Z9847wrTuvN/f52v\nv2tBpzAjD9xWIe4MGz8kdztOH82t7I8vzm/OnBQQPZDXLGWCr6aF2qbxCKyhcT2f\n1JsqJShUATcoPoD45bI1Pxa3zaObolknHOL5Ec2lPfyWKa9ml9m0muydWuTwgkfF\nBKcB7cqO6/HItfbbO29RWEpLOxUuug/YBkOgUvC5pVn+Q0WnZnhlgj5etkWTgZ+g\nLMdPV7RAZkJqvmp3gyAMcImglJ9kBMgEUE7dbTMiPQKBgQDLj1Vy/WdAfnBBeOPl\noWQwelS03BP1y+bBqsjM9LtW5ubuK6D0sWzBMxyfBOcCCyNVkld+Uhn4rT7ttlSh\nnq30qfrgrY4LK/oGJgl4ItFIsTLDcubPrOzYsmtCVx0sjLEbpx0B7eN84Dy4CQ30\nahb4I8yaS9dujk/PF+b33cTvPQKBgQDLYxeHcI9rn82w/ZbqsByWcj+vs+rq8Tj6\nu6Lg+4saToTx+q3A6h72zStCrxgExDmwicgRbXMdHaw35spicfllag5yC317sge0\nZID9dfILrodPItDseUTCH0rcLV957TKxHdbTRXgpJO12cR6nWWFfdpTApUattaM6\nwKMNVzatKwKBgH/qZ1jmMl8N1T7fV96yB4KLLPWmIojgA/cJejuJQrw+Rzm9gtyh\nLeVOWBr5eq0HEMIyPvbrYtSpEZVRcWdKjqfjtPpBNbSBgxEgD+1Fe9xBNdQqPS6d\nzQvzOoGqunOHSbhF0G2o7r5HLwC69+DyVdLCtEz1gcBAf5h0vbhezOVFAoGAUpzB\nZeY2m0ShzA51SoaWVhW6AKhX925b8pxcoH4MFndmUNO91+pBAYZ81+0/GFCMDkUo\n2stWQrFWzq4T7sDUyHi3vWMlaST7NC7OpiVOV6hglahSrHrljuQfCCf17BSIzqCM\nLey9zhiNy9pO1TLc9mW4Ljfz6YMp9idhE34lW1kCgYEApxGtB07bHZRjaOvsp+HR\nCuUBkcYXrSJgiNOpzqExLDIIpcc8nmu/XfBJXvBV9qmPsiafnDqoqIZPDYR1axIE\ncD8uRjgq8zIG6D6lNUj4YV0oknnsN0oOknThVOX+swy5hbtM0P803kNFNZqn15EF\nKOn7umOiQr3IQApOAEmU5BM=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-24d2f@bot-discord-ggz.iam.gserviceaccount.com",
+    "client_id": "105859256884377428476",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-24d2f%40bot-discord-ggz.iam.gserviceaccount.com"
+  }),
   databaseURL: "https://bot-discord-ggz.firebaseio.com"
 });
 // As an admin, the app has access to read and write all data, regardless of Security Rules
